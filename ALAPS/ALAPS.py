caa0c3ce3d6769f0727bd7440162f069d8199a61
ditmann <adriandenutrolige@gmail.com>
2023-03-16 20:08:15 +0100

    Starten på boxer

--- ALAPS/ALAPS.py
+++ ALAPS/ALAPS.py
@@ -1,4 +1,26 @@
 import subprocess
+import tkinter
+import customtkinter
+
+
+
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("400x240")
+
+
+
+
+
+
+
+
+
+
+app.mainloop()
 
 def comp(AssetTag):
     command = f"powershell -Command \"$password = (Get-AdmPwdPassword -ComputerName {AssetTag}).Password; Write-Output $password\""
@@ -9,5 +31,20 @@ def comp(AssetTag):
     else:
         return("Error, failed to retrieve password.")
 
-ComputerName = input("Input Asset-TAG:\n")
+ComputerName = OD 
 print(comp(ComputerName))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
