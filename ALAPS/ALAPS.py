ad96651636bccdd180fc58fad99c869dd98c4402
AKrokaa <andreas@krokaa.no>
2023-05-08 14:07:39 +0200

    work :)

--- ALAPS/ALAPS.py
+++ ALAPS/ALAPS.py
@@ -3,23 +3,28 @@ import tkinter
 import customtkinter
 import pyautogui
 
-
-
-
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
-
 
 app = customtkinter.CTk()  # Lager boks
 app.geometry("390x200")
 
-LapsPass = "Dette er ett passord"
-
+label = customtkinter.CTkLabel(app, text="",font=('comic sans', 24,'bold'))
+label.place(relx=0.4, rely=0.4, anchor=tkinter.CENTER)
 
 #denne skal finne laps passordet og putte det inn i en variabel (LapsPass)
-def FinnPass():
-    print(entry.get())
+def FinnPass(AssetTag):
+    command = f"powershell -Command \"$password = (Get-AdmPwdPassword -ComputerName {AssetTag}).Password; Write-Output $password\""
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    global LapsPass
 
+    if result.returncode == 0:
+        LapsPass = result.stdout.strip()
+        label.configure(text=LapsPass)
+    else:
+        label.configure(text="Error, failed to retrieve password.")
+
+    #label.configure(text= LapsPass)
 
 #Skriver passordet i variablen LapsPass
 def SkrivPass():
@@ -31,38 +36,16 @@ def SkrivPass():
     pyautogui.write(LapsPass)
     pyautogui.press("enter")
 
-
-
-
-#lager søke etter Laps passord knapp
-KnappFinn = customtkinter.CTkButton(master=app,
-                                 width=50,
-                                 height=32,
-                                 border_width=0,
-                                 corner_radius=8,
-                                 text="SØK!",
-                                 command=FinnPass
-                                 )
-KnappFinn.place(relx=0.85, rely=0.1, anchor=tkinter.CENTER) #plassere Søk knappen i boksen
-
-
-
-
 #Denne knappen er for å skrive 
 KnappSkriv = customtkinter.CTkButton(master=app,
                                  width=50,
                                  height=32,
                                  border_width=0,
                                  corner_radius=8,
-                                 text="Skriv!",
+                                 text="Print",
                                  command=SkrivPass
                                  )
 KnappSkriv.place(relx=0.85, rely=0.4, anchor=tkinter.CENTER)
-
-
-
-
-
 
 #Lager Asset Tag input boks
 entry = customtkinter.CTkEntry(master=app,
@@ -74,31 +57,22 @@ entry = customtkinter.CTkEntry(master=app,
                                )                               
 entry.place(relx=0.4, rely=0.1, anchor=tkinter.CENTER) # plaserer Asset Tag boks
 
-
-
-
+#lager søke etter Laps passord knapp
+KnappFinn = customtkinter.CTkButton(master=app,
+                                 width=50,
+                                 height=32,
+                                 border_width=0,
+                                 corner_radius=8,
+                                 text="Søk!",
+                                 command=lambda: FinnPass(entry.get())
+                                 )
+KnappFinn.place(relx=0.85, rely=0.1, anchor=tkinter.CENTER) #plassere Søk knappen i boksen
 
 #Label som viser lapspassordet
-label = customtkinter.CTkLabel(app, text=LapsPass, fg_color="green",font=('comic sans', 24,'bold'))
-label.place(relx=0.4, rely=0.4, anchor=tkinter.CENTER)
 
-
-
-
+#label.configure(text=LapsPass)
 
 app.mainloop()
-
-
-#Søker etter Laps passord og putter det i en variabel
-
-#def comp(AssetTag):
- #   command = f"powershell -Command \"$password = (Get-AdmPwdPassword -ComputerName {AssetTag}).Password; Write-Output $password\""
-  #  result = subprocess.run(command, shell=True, capture_output=True, text=True)
-   # 
-    #if result.returncode == 0:
-    #    return(result.stdout.strip())
-    #else:
-     #   return("Error, failed to retrieve password.")
 
 #ComputerName = input("Input Asset-TAG:\n")
 #print(comp(ComputerName))
