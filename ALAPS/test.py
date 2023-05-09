cc8dccc6814541929b617a1d531a2a775eeb62cd
AKrokaa <andreas@krokaa.no>
2023-05-09 09:52:03 +0200

    Testing

---
+++ ALAPS/test.py
@@ -0,0 +1,12 @@
+def test(x):
+    if x == "1":
+        print("WOAH, 1!")
+    elif x == "2":
+        print("WOW, 2?!?")
+    else:
+        print("Dust")
+
+val = input("Si ett tall?")
+val2 = val + 1
+test(val)
+
