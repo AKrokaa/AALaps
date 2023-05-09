9129043702b2f2c2d6e1ae4b0715019e6ea9559e
AKrokaa <andreas@krokaa.no>
2023-05-09 11:32:07 +0200

    Fixed checkmark functions.

--- ALAPS/ALAPS.py
+++ ALAPS/ALAPS.py
@@ -19,12 +19,12 @@ app.title("(☞ﾟヮﾟ)☞ AALAPS ☜(ﾟヮﾟ☜)")
 app.geometry("390x200")
 
 label = customtkinter.CTkLabel(app, text="",font=('comic sans', 24,'bold'))
-label.place(relx=0.4, rely=0.7, anchor=tkinter.CENTER)
+label.place(relx=0.4, rely=0.5, anchor=tkinter.CENTER)
 
 def theme(x):
-    if x == 1:
+    if x == "1":
         customtkinter.set_appearance_mode("dark")
-    elif x == 0:
+    elif x == "0":
         customtkinter.set_appearance_mode("light")
 
 #denne skal finne laps passordet og putte det inn i en variabel (LapsPass)
@@ -39,25 +39,40 @@ def FinnPass(AssetTag):
     else:
         label.configure(text="Error, failed to retrieve password.")
 
-
 #Skriver passordet i variablen LapsPass
 def SkrivPass(x, y):
     
-    if y == 1:
+    if y == "1":
         time.sleep(3)
     else:
         pyautogui.keyDown("alt")
         pyautogui.press("tab")
         pyautogui.keyUp("alt")
 
-    if x == 1:
+    if x == "1":
         pyautogui.write('.\Administrator')
         pyautogui.press("tab")
         pyautogui.write(LapsPass)
         pyautogui.press("enter")
-    elif x == 0:
+    elif x == "0":
         pyautogui.write(LapsPass)
         pyautogui.press("enter")
+
+def checkbox_event():
+    print("checkbox toggled, current value:", check_var_sek.get())
+
+check_var_sek = customtkinter.StringVar(value="0")
+checkbox_sek = customtkinter.CTkCheckBox(app, text="3 sek", command=checkbox_event,
+                                     variable=check_var_sek, onvalue="1", offvalue="0")
+checkbox_sek.place(relx=0.16, rely=0.9, anchor=tkinter.CENTER)
+
+def checkbox_event():
+    print("checkbox toggled, current value:", check_var_barepass.get())
+
+check_var_barepass = customtkinter.StringVar(value="0")
+checkbox_barepass = customtkinter.CTkCheckBox(app, text="Bare Pass", command=checkbox_event,
+                                     variable=check_var_barepass, onvalue="1", offvalue="0")
+checkbox_barepass.place(relx=0.34, rely=0.9, anchor=tkinter.CENTER)
 
 #Denne knappen er for å skrive 
 KnappSkriv = customtkinter.CTkButton(master=app,
@@ -66,9 +81,9 @@ KnappSkriv = customtkinter.CTkButton(master=app,
                                  border_width=0,
                                  corner_radius=8,
                                  text="Skriv",
-                                 command=SkrivPass
+                                 command=lambda: SkrivPass(check_var_barepass.get(), check_var_sek.get())
                                  )
-KnappSkriv.place(relx=0.85, rely=0.7, anchor=tkinter.CENTER)
+KnappSkriv.place(relx=0.85, rely=0.5, anchor=tkinter.CENTER)
 
 #Lager Asset Tag input boks
 entry = customtkinter.CTkEntry(master=app,
@@ -91,23 +106,10 @@ KnappFinn = customtkinter.CTkButton(master=app,
                                  )
 KnappFinn.place(relx=0.85, rely=0.25, anchor=tkinter.CENTER) #plassere Søk knappen i boksen
 
-def checkbox_event():
-    print("checkbox toggled, current value:", check_var_sek.get())
-
-check_var_sek = customtkinter.StringVar(value="off")
-checkbox_sek = customtkinter.CTkCheckBox(app, text="3 sek", command=checkbox_event,
-                                     variable=check_var_sek, onvalue="on", offvalue="off")
-checkbox_sek.place(relx=0.16, rely=0.9, anchor=tkinter.CENTER)
-
-
-def checkbox_event():
-    print("checkbox toggled, current value:", check_var_barepass.get())
-
-check_var_barepass = customtkinter.StringVar(value="off")
-checkbox_barepass = customtkinter.CTkCheckBox(app, text="Bare Pass", command=checkbox_event,
-                                     variable=check_var_barepass, onvalue="on", offvalue="off")
-checkbox_barepass.place(relx=0.34, rely=0.9, anchor=tkinter.CENTER)
-
+"""
+Ønsker bruk av customtkinter.CTkSwitch som sender opp til function theme(x)
+"1" er dark-mode, "2" er light-mode
+"""
 
 #dette er slik att boksen looper
 app.mainloop()
